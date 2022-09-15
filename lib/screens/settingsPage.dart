adc383ed438d6cf8c6ae0c3418047ec050e1fb07
GAsplund <gasplund2@gmail.com>
2022-09-15 23:39:36 +0200

    Add AppBar to settings page

--- lib/screens/settingsPage.dart
+++ lib/screens/settingsPage.dart
@@ -18,51 +18,54 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Settings"),
+        ),
         body: ListView(children: [
-      Text("Settings"),
-      /*SwitchListTile(
+          Text("Settings"),
+          /*SwitchListTile(
         value: true,
         title: Text(
           "Dark Theme",
         ),
         onChanged: (bool value) {},
       ),*/
-      ListTile(
-        title: Text("Theme"),
-        trailing: DropdownButton(
-          value: settings.currentTheme.index.toString(),
-          items: [
-            DropdownMenuItem(
-              value: "2",
-              child: RichText(
-                  text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
-                WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.wb_sunny))),
-                TextSpan(text: "Light")
-              ])),
+          ListTile(
+            title: Text("Theme"),
+            trailing: DropdownButton(
+              value: settings.currentTheme.index.toString(),
+              items: [
+                DropdownMenuItem(
+                  value: "2",
+                  child: RichText(
+                      text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
+                    WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.wb_sunny))),
+                    TextSpan(text: "Light")
+                  ])),
+                ),
+                DropdownMenuItem(
+                  value: "1",
+                  child: RichText(
+                      text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
+                    WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.wb_incandescent))),
+                    TextSpan(text: "Dark")
+                  ])),
+                ),
+                DropdownMenuItem(
+                  value: "0",
+                  child: RichText(
+                      text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
+                    WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.settings))),
+                    TextSpan(text: "System")
+                  ])),
+                ),
+              ],
+              onChanged: (String value) {
+                settings.setCurrentTheme(ThemeMode.values[int.tryParse(value)], theming);
+                setState(() {});
+              },
             ),
-            DropdownMenuItem(
-              value: "1",
-              child: RichText(
-                  text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
-                WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.wb_incandescent))),
-                TextSpan(text: "Dark")
-              ])),
-            ),
-            DropdownMenuItem(
-              value: "0",
-              child: RichText(
-                  text: TextSpan(style: Theme.of(context).textTheme.bodyText1, children: [
-                WidgetSpan(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 2.0), child: Icon(Icons.settings))),
-                TextSpan(text: "System")
-              ])),
-            ),
-          ],
-          onChanged: (String value) {
-            settings.setCurrentTheme(ThemeMode.values[int.tryParse(value)], theming);
-            setState(() {});
-          },
-        ),
-      )
-    ]));
+          )
+        ]));
   }
 }
