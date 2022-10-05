c63c54bd5f0e0b3e2bcee30b9ba3c09471433f8c
GAsplund <gasplund2@gmail.com>
2022-10-05 23:46:04 +0200

    Add documentation

--- packages/timeedit/lib/utilities/timeedit_web.dart
+++ packages/timeedit/lib/utilities/timeedit_web.dart
@@ -3,11 +3,18 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart' as dom;
+import 'package:timeedit/objects/category.dart';
 
+const linkbase = "https://cloud.timeedit.net/";
+
+/// A class for interacting with TimeEdit web endpoints.
 class TimeEditWeb {
-  /// Gets a Schedule object from given parameters
-  static Future<Map<String, dynamic>> getSchedule(String org, String entry, int id, List<String> objects) async {
-    String url = "https://cloud.timeedit.net/$org/$entry/ri.json?sid=$id";
+  /// Gets a Schedule object at [org]/[entry]/[pageId]
+  /// using [objects] as filters.
+  ///
+  /// Returns a JSON object with the schedule.
+  static Future<Map<String, dynamic>> getSchedule(String org, String entry, int pageId, List<String> objects) async {
+    String url = linkbase + "$org/$entry/ri.json?sid=$pageId";
 
     // Add schedule objects
     url += "&objects=";
@@ -19,27 +26,37 @@ class TimeEditWeb {
     return jsonDecode(await _getURLRaw(url));
   }
 
-  /// Gets a list of schedule objects from given parameters
-  static getObjects(String org, String entry, List<int> categories, List<String> filters) {
-    String url = "https://cloud.timeedit.net/$org/$entry/objects.html";
+  /// Gets a list of schedule objects at [org]/[entry]/[pageId].
+  ///
+  /// Returns a list of [String] that are the schedule objects.
+  static List<String> getObjects(String org, String entry, List<int> categories, List<String> filters) {
+    String url = linkbase + "$org/$entry/objects.html";
+    return [];
   }
 
-  static getCategoriesAndFilters(String org, String entry) {
-    String url = "https://cloud.timeedit.net/$org/$entry/ri.html";
+  /// Gets all available categories from [org]/[entry]/[pageId]
+  ///
+  /// Returns a list of [Category] objects.
+  static List<Category> getCategories(String org, String entry, int pageId) {
+    String url = linkbase + "$org/$entry/ri.html?sid=$pageId";
+    return [];
   }
 
-  /// Gets all available categories from given parameters
-  static Map<String, int> getCategories(String org, String entry) {
-    String url = "https://cloud.timeedit.net/$org/$entry/ri.html";
+  /// Gets a list of page ids on [org]/[entry]
+  ///
+  /// Returns a list of [int] that are the page ids.
+  static List<int> getPageIds(String org, String entry) {
+    String url = linkbase + "$org/$entry/s.html";
+    return [];
   }
 
-  static _getCategories(dom.Document doc) {}
-
-  static List<dom.Element> getFilters(String org, String entry) {
-    String url = "https://cloud.timeedit.net/$org/$entry/ri.html";
+  /// Gets a list of organisations given a [query] search.
+  ///
+  /// Returns a JSON object with the organisation results.
+  static Future<Map<String, dynamic>> searchOrgs(String query) async {
+    String url = "https://www.timeedit.net/v1/search/web?term=$query";
+    return jsonDecode(await _getURLRaw(url));
   }
-
-  static _getFilters(dom.Document doc) {}
 
   static Future<String> _getURLRaw(String url) async {
     http.Response response = await http.get(Uri.parse(url));
