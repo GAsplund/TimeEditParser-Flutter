cb71aeccb8e7bda1ca793a4e61a0342e52060c59
GAsplund <gasplund2@gmail.com>
2022-10-08 13:38:45 +0200

    Try to get entries on create

--- packages/timeedit/lib/utilities/org_start.dart
+++ packages/timeedit/lib/utilities/org_start.dart
@@ -1,13 +1,24 @@
+import 'package:timeedit/objects/org_entry.dart';
+import 'package:timeedit/src/timeedit_web.dart';
 import 'package:timeedit/utilities/schedule_search_builder.dart';
 
 /// A class for building a [ScheduleSearchBuilder] object.
 class OrgStart {
-  List<String> entries = [];
+  List<OrgEntry> entries = [];
   String org;
 
   late String entry;
 
-  OrgStart(this.org);
+  OrgStart._create(this.org);
+
+  static Future<OrgStart> create(String org) async {
+    OrgStart component = OrgStart._create(org);
+    return component;
+  }
+
+  Future getEntries() async {
+    entries = await TimeEditWeb.getEntries(org);
+  }
 
   /// Builds a [ScheduleSearchBuilder] object.
   ScheduleSearchBuilder build() {
